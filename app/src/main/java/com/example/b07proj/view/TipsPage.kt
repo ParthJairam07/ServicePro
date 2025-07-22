563195205310b7b48134d44db1491311ade854e0
AbinashNagendran <abinashnagendran@gmail.com>
2025-07-22 18:52:39 -0400

    feat/implemented full UI TipsPage

--- app/src/main/java/com/example/b07proj/view/TipsPage.kt
+++ app/src/main/java/com/example/b07proj/view/TipsPage.kt
@@ -1,17 +1,24 @@
 package com.example.b07proj.view
 
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -23,8 +30,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.google.gson.Gson
 import com.google.gson.JsonElement
@@ -32,7 +44,14 @@ import com.google.gson.JsonObject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import com.example.b07proj.R
+import com.example.b07proj.ui.theme.Primary40
 import java.io.InputStreamReader
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.b07proj.ui.theme.backgroundAccent
 
 
 // get user answers in a usable form for lazyColumn
@@ -80,9 +99,15 @@ object AnswersProvider {
 // again another blueprint for structure of questions.json
 private data class SafetyPlanData(val tips: JsonObject)
 
+data class TipAndTitle(
+    val tip: String,
+    val title: String
+)
+
 // return list of generated tips based on answers
-private fun generateTips(planData: SafetyPlanData, userAnswers: Map<String, JsonElement>): List<String> {
-    val generatedTips = mutableListOf<String>()
+private fun generateTips(planData: SafetyPlanData, userAnswers: Map<String, JsonElement>): List<TipAndTitle> {
+    // a list to store a tip followed by its title
+    val generatedPairs  = mutableListOf<TipAndTitle>()
     userAnswers.forEach { (questionId, answerElement) ->
         val tip = findTipForAnswer(
             planData.tips,
@@ -91,13 +116,15 @@ private fun generateTips(planData: SafetyPlanData, userAnswers: Map<String, Json
             userAnswers
         )
         if (tip != null) {
-            generatedTips.add(tip)
-            println("Error when finding correct tip")
+            val title = getTipTitle(questionId)
+            generatedPairs.add(TipAndTitle(title = title, tip = tip))
+
         }
     }
-    return generatedTips
+    return generatedPairs
 
 }
+
 // based on answer, find the tip for that answer
 private fun findTipForAnswer(
     tipsRoot: JsonObject,
@@ -149,6 +176,28 @@ private fun findTipForAnswer(
         else -> null
     }
 }
+// return the header string associated to the question id
+private fun getTipTitle(questionIdStr: String): String {
+    return when (questionIdStr) {
+        "1" -> "Stay Prepared"
+        "2" -> "Local Support"
+        "3" -> "Stock Your Safe Room with Essentials"
+        "4" -> "Household Safety"
+        "5" -> "Plan Around Your Children"
+        "6" -> "Reach Out During Abuse"
+        "7" -> "Start Logging Incidents"
+        "8" -> "Save Emergency Contacts"
+        "9" -> "Plan Your Leaving Time"
+        "10" -> "Ready a Go-Bag"
+        "11" -> "Secure Emergency Money"
+        "12" -> "Confirm a Safe Shelter Option"
+        "13" -> "Stay Alert About Any Ongoing Contact"
+        "14" -> "Share Your Protection Order"
+        "15" -> "Keep Safety Tools Ready"
+        "16" -> "Seeking Support"
+        else -> "Something went wrong. Sorry!"
+    }
+}
 private fun extractAnswerData(questionIdStr: String, answerElement: JsonElement): Pair<String, String?> {
     // for when the answer JSON is just a primitive, simple answer given no follow up
     if (answerElement.isJsonPrimitive) return Pair(answerElement.asString, null)
@@ -179,8 +228,8 @@ private fun JsonObject.getString(key: String): String? =
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TipPage(navController: NavHostController) {
-    // create a state to hold all tips , default is empty list
-    var tipsList by remember { mutableStateOf<List<String>>(emptyList()) }
+    // create a state to hold all tipsAndTitle objects, default is empty list
+    var tipsList by remember { mutableStateOf<List<TipAndTitle>>(emptyList()) }
     // to see if the data is still loading or not
     var isLoading by remember { mutableStateOf(true) }
     // get application context, used to read a file
@@ -208,15 +257,48 @@ fun TipPage(navController: NavHostController) {
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Your Personalized Safety Plan") })
+            // ordered as a column for horizontal line below the header
+            Column {
+                TopAppBar(
+                    title = {
+                        // logo of the app
+                        Image(
+                            painter = painterResource(R.drawable.templogo) ,
+                            contentDescription = stringResource(id = R.string.logoDescription),
+                        )
+                    }
+                )
+                HorizontalDivider(
+                    color = Color.Gray,
+                    thickness = 0.5.dp
+                )
+            }
+        }, floatingActionButton = {
+            ExitButton(
+                modifier = Modifier
+                    .padding(5.dp)    // placement
+            )
         }
     ) { paddingValues ->
+        // used to load all tips in a scrollable view (very similar to recycler view)
         LazyColumn(
             modifier = Modifier.padding(paddingValues).fillMaxSize(),
             contentPadding = PaddingValues(16.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            item {
+                val myFont = FontFamily(Font(R.font.afacad))
+                Text(
+                    text = stringResource(R.string.tipsHeader),
+                    color = backgroundAccent,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = myFont
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+            }
             if (isLoading) {
                 // If we are loading, show a spinner.
                 item {
@@ -225,23 +307,66 @@ fun TipPage(navController: NavHostController) {
             } else {
                 // Once loading is false, show the list.
                 items(tipsList) { tipText ->
-                    TipCard(tip = tipText)
+                    TipCard(tipAndTitle = tipText)
                 }
+            }
+            item {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    BackButton(
+                        navController = navController
+                    )
+                }
+
             }
         }
     }
 }
 // display for one tip (for later, make the UI better)
 @Composable
-fun TipCard(tip: String) {
+fun TipCard(tipAndTitle: TipAndTitle) {
     Card(
         modifier = Modifier.fillMaxWidth(),
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
+        // header + tip text
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = tipAndTitle.title,
+                color = backgroundAccent,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = myFont
+
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = tipAndTitle.tip,
+                fontFamily = myFont
+            )
+
+        }
+    }
+}
+@Composable
+fun BackButton(navController: NavHostController) {
+    Button(
+        onClick = {
+            navController.navigate("safety_plan_tips")
+        },
+        colors = ButtonDefaults.buttonColors(containerColor = Primary40),
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.whitearrowgoback),
+            contentDescription = stringResource(id = R.string.arrow_content_description),
+            modifier = Modifier.size(16.dp)
+        )
+        Spacer(modifier = Modifier.width(12.dp))
         Text(
-            text = tip,
-            modifier = Modifier.padding(16.dp),
-            style = MaterialTheme.typography.bodyLarge
+            text = stringResource(R.string.BackButtonText),
+
         )
     }
 }
