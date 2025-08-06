ee727e42548ce0fa2da32a7dc5601cface735490
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-08-06 16:03:09 -0400

    feat: added a image in home page

--- app/src/main/java/com/example/b07proj/view/HomePage.kt
+++ app/src/main/java/com/example/b07proj/view/HomePage.kt
@@ -1,6 +1,8 @@
 package com.example.b07proj.view
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.content.MediaType.Companion.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -92,6 +95,13 @@ fun UIHomePage(navController: NavHostController) {
                             "direct_links"
                         )
                     }
+                    Image(
+                        painter = painterResource(id = R.drawable.relationshipssimple),
+                        contentDescription = "Help support image",
+                        modifier = Modifier
+                            .padding(top = 32.dp)
+                            .size(180.dp)
+                    )
                 }
             }
         }
